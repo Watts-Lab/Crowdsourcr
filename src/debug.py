6b6de6b169d371bde72336aa97a5b393b9385090
Markus Mobius <markusmobius@gmail.com>
2021-03-14 22:40:35 -0400

    Fixes

--- src/debug.py
+++ src/debug.py
@@ -10,36 +10,49 @@ sys.path.pop(0)
 config_filename="config_jared.json.production"
 do_implement=False
  
+
+def mapIsoName(name):
+    frags=name.split('_')
+    if frags[-1]=="dem":
+        frags[-1]="rep"
+        return frags.join('_')
+    else:
+        if frags[-1]=="rep":
+            frags[-1]="dem"
+            return frags.join('_')
+        else:
+            return None
+
+
+
 def main():
     app_config.populate_config(config_filename)
     db = pymongo.MongoClient()[app_config.db_name]
     print("MODULES:")  
     d=db.ctypes.find({},{'name':1})
     for row in d:
-        print(row["name"]) 
-        if do_implement:
-            if row["name"]=="numbers":   
-                query = { "name": row["name"] }
-                newvalues = { "$set": { "isomorphicModule": "numbersreverse" } }
+        isoName=mapIsoName(row["name"])
+        if isoName!=None:
+            query = { "name": row["name"] }
+            newvalues = { "$set": { "isomorphicModule": isoName } }
+            if do_implement:
                 db.ctypes.update_one(query, newvalues)  
-            if row["name"]=="numbersreverse":   
-                query = { "name": row["name"] }
-                newvalues = { "$set": { "isomorphicModule": "numbers" } }
-                db.ctypes.update_one(query, newvalues)  
+            print("setting isomorphicModule of module "+row["name"]+" to "+isoName)
+        else:
+            print("skipping module "+row["name"])
 
     print("TASKS:")  
     d=db.ctasks.find({},{'taskid':1})
     for row in d:
-        print(row["taskid"]) 
-        if do_implement:          
-            if row["taskid"]=="1":   
-                query = { "taskid": row["taskid"] }
-                newvalues = { "$set": { "isomorphicTask": "2" } }
+        isoName=mapIsoName(row["taskid"])
+        if isoName!=None:
+            query = { "taskid": row["taskid"] }
+            newvalues = { "$set": { "isomorphicTask": isoName } }
+            if do_implement:
                 db.ctasks.update_one(query, newvalues)  
-            if row["taskid"]=="2":   
-                query = { "taskid": row["taskid"] }
-                newvalues = { "$set": { "isomorphicTask": "1" } }
-                db.ctasks.update_one(query, newvalues)  
+            print("setting isomorphicTask of task "+row["taskid"]+" to "+isoName)
+        else:
+            print("skipping task "+row["taskid"])
 
 if __name__ == "__main__":
     main()
